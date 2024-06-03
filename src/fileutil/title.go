56ef63c5fe0e3b4e14a0a90e3f96f30dd3d77420
FredPont <fred.softwares@gmail.com>
2024-06-03 10:15:11 +0200

    Version update

--- src/fileutil/title.go
+++ src/fileutil/title.go
@@ -24,7 +24,7 @@ func Title() {
 	fmt.Println("")
 	fmt.Println("   ┌──────────────────────────────────────────┐") // unicode U+250C
 	fmt.Println("   │ Compare Footprint Lists (c)Frederic PONT │")
-	fmt.Println("   │     v20240530 - Free Software GNU GPL    │")
+	fmt.Println("   │     v20240603 - Free Software GNU GPL    │")
 	fmt.Println("   └──────────────────────────────────────────┘")
 	//fmt.Println("")
 }
